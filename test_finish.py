5265bfc5b0c781da759308c04296111690aaffcb
Danko <116387462+DankoMoscow@users.noreply.github.com>
2023-03-05 19:18:04 +0300

    Add files via upload

--- test_finish.py
+++ test_finish.py
@@ -18,6 +18,12 @@ pio.templates
 
 pid = os.getpid()
 hv.extension('plotly')
+# radio_group = pn.widgets.RadioButtonGroup(
+#     name='Radio Button Group', options=['Основные параметры', 'Дополнительные параметры'], button_type='success') #тут будет вкладка с параметрами
+radio_group = pn.widgets.Select(
+    value='Основные параметры',
+    options=['Основные параметры', 'Дополнительные параметры']
+)
 
 float_width = pn.widgets.FloatSlider(name='Ширина образцов', start=0.01, end=0.1, step=0.01, value=0.01,
                                      format=PrintfTickFormatter(format='%.2f м'))
@@ -29,6 +35,7 @@ float_height = pn.widgets.FloatSlider(name='Высота образца', start=
 
 float_volume = pn.widgets.FloatSlider(name='Объём аппарата', start=0.1, end=1, step=0.1, value=0.3,
                                       format=PrintfTickFormatter(format='%.2f кубических метров'))
+
 float_flowrate = pn.widgets.FloatSlider(name='Объёмный расход', start=0.0001, end=0.01, step=0.001, value=0.001,
                                         format=PrintfTickFormatter(format='%.4f кубических метров в секунду'))
 
@@ -40,6 +47,8 @@ float_diff_coef = pn.widgets.FloatSlider(name='Коэффициент диффу
 int_number_samples = pn.widgets.IntSlider(name='Количество образцов', start=100, end=1000, step=100, value=200,
                                           format=PrintfTickFormatter(format='%.1f штук'))
 
+
+
 group_of_key = pn.widgets.RadioButtonGroup(
     name='Выбор необходимого вида образца', options=['one_dim', 'cyl', 'sphere'], button_type='success',
     orientation='vertical')
@@ -49,30 +58,49 @@ group_of_ways = pn.widgets.RadioButtonGroup(
 
 groups = pn.Row(group_of_key, group_of_ways)
 
+groups_main = pn.Column(float_width, float_length, float_height, float_volume, float_flowrate, float_dt, float_diff_coef, int_number_samples, groups)
+
+
 static_cond = pn.widgets.StaticText(name='Условие устойчивости', value=' ')
 static_text = pn.widgets.StaticText(name='Поле для вывода ошибок', value=' ')
 static_time = pn.widgets.StaticText(name='Время расчёта', value=' ')
 static_time_process = pn.widgets.StaticText(name='Расчётное время проведения сушки в часах', value=' ')
+static_time_process_result = pn.widgets.StaticText(name='Итог сушки', value=' ')
+
+
 
 button = pn.widgets.Button(name='Нажмите для запуска расчёта', button_type='primary')
 button_exit = pn.widgets.Button(name='Нажмите для выхода', button_type='primary')
 
-# виджеты для отображения
-main_column = pn.Column('# Расчёт процесса сверхкритической сушки', float_width, float_length, float_height,
-                        float_diff_coef, float_volume, float_flowrate, float_dt, \
-                        int_number_samples, groups, static_text, static_cond, static_time,
-                         static_time_process, button, button_exit)
+
+
+#TODO доп параметры хер знает зачем, хер знает почему, но да ладно/ необходимо сделать так, чтобы при нажатии вкладки доп параметров сворачивалась вкладка с основными и переходила на доп и обратно, с полным сохранением всех value
+'''
+таким образом диалоговое окно будет меньше по размеру и удобнее
+'''
+pressure_select = pn.widgets.FloatSlider(name='Давление системы', start=80, end=200, step=5, value=120,
+                                          format=PrintfTickFormatter(format='%.1f бар'))
+temperature_select = pn.widgets.FloatSlider(name='Температура системы', start=300, end=500, step=1, value=313,
+                                          format=PrintfTickFormatter(format='%.1f кельвинов'))
+
+dop_column = pn.Column(pressure_select, temperature_select)
+# TODO окончание говна
+
+
+#виджеты для отображения
+
+
+main_column = pn.WidgetBox('# Расчёт процесса сверхкритической сушки', radio_group, groups_main,
+                          static_text, static_cond, static_time,
+                          static_time_process, static_time_process_result,  button, button_exit)
 
 plot = hv.Curve([0]).opts(width=300)
-# общий виджет
-main_window = pn.Row(pn.Spacer(width=100), main_column, pn.Spacer(width=50), plot, sizing_mode='stretch_width')
+main_window = pn.Row(pn.Spacer(width=100), main_column, pn.Spacer(width=50), plot, sizing_mode='stretch_width') # общий виджет
 
 
-def onClose(event):
-    # убивает процесс
+def onClose(event): # убивает процесс
     static_text.value = 'До новых встреч'
     panel.command.die('By')
-
 
 def get_condition():
     if cond_scheme == 5:
@@ -80,10 +108,11 @@ def get_condition():
     else:
         static_cond.value = 'Условие выполянется'
 
-
 def get_time_drying(n):
     static_time_process.value = n * float_dt.value / 3600
 
+def view_end_process(slovo):
+    static_time_process_result.value = slovo
 
 def work_process():
     if podskazka == 5:
@@ -91,18 +120,18 @@ def work_process():
     else:
         static_text.value = 'Всё работает правильно'
 
-
 def get_time():
     static_time.value = delta_time
-
 
 def show_time_process(list):
     condition_stop = list[0] * 0.05
     for n, value in enumerate(list):
-        #print(value/list[0])
         if value <= condition_stop:
+            slovo = 'Сушка проведена успешно'
             break
-    return n
+        else:
+            slovo = 'Время сушки недостаточно. Содержание спирта в образцах выше запланированного'
+    return n, slovo
 
 def run(event):
     start_time = datetime.now()
@@ -110,15 +139,16 @@ def run(event):
     matrix_of_c, list_of_mass, c_app, time, i, r_list, podskazka, cond_scheme = fick_classes.main(float_width.value, float_length.value, float_height.value,
         float_volume.value, float_flowrate.value, float_dt.value, float_diff_coef.value, int_number_samples.value, group_of_key.value, group_of_ways.value, static_text.value,static_cond.value)
 
-    n = show_time_process(list_of_mass)
+    n, slovo = show_time_process(list_of_mass)
     get_time_drying(n)
+    view_end_process(slovo)
     template = "plotly_white"
 
     plot_mass = go.Figure(data = go.Scatter(y = list_of_mass, x = time/3600))
     plot_mass.update_layout(title="График изменения массы", font = dict(family = "Overpass"), height=500,width=500, template = template,
                       xaxis_title= 'Время, ч', yaxis_title='Масса спирта в образцах, кг')
-    plot_mass.update_xaxes(gridcolor='LightPink')
-    plot_mass.update_yaxes(gridcolor='LightPink')
+    plot_mass.update_xaxes(gridcolor='Black')
+    plot_mass.update_yaxes(gridcolor='Black')
 
     time_ratio = 100
     plot_conc  = go.Figure()
@@ -126,8 +156,8 @@ def run(event):
         plot_conc.add_trace(go.Scatter(y=n, x = r_list,name=str(l)+' шаг'))
     plot_conc.update_layout(title="График изменения концентрации", font = dict(family = "Overpass"), height=500,width=500,template = template,
                       xaxis_title='Радиус образцов, м', yaxis_title='Концентрация спирта в образцах, кг/м3')
-    plot_conc.update_xaxes(gridcolor='LightPink')
-    plot_conc.update_yaxes(gridcolor='LightPink')
+    plot_conc.update_xaxes(gridcolor='Black')
+    plot_conc.update_yaxes(gridcolor='Black')
 
     plot_3d = go.Figure(data=[go.Surface(x=r_list, y=time, z=matrix_of_c)])
     plot_3d.update_layout(title="График отображения 3D концентрации", font = dict(family = "Overpass"), template = template, scene=dict(xaxis_title='Радиус, м',yaxis_title='Время, с',
@@ -153,6 +183,5 @@ def main():
     button.on_click(run)
     button_exit.on_click(onClose)
     main_window.show()
-
 
 main()
