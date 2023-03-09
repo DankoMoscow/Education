a049f22e4f505b7605107f588a793c7333312d60
Danko <116387462+DankoMoscow@users.noreply.github.com>
2023-03-09 15:39:21 +0300

    Add files via upload

--- fick_classes.py
+++ fick_classes.py
@@ -4,10 +4,12 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 import os
 import math
+from scipy import optimize
+from scipy.optimize import minimize
 
-
+target_material_porosity = 120 #кг/м3
 porosity = 0.95
-tau_izv = 5.5 #извилистость пор
+tau_izv = 11 #5.5 #извилистость пор
 
 Vcr_ips = 220 #см3/моль - критический молярный объём ips
 Vcr_co2 = 94.07
@@ -55,21 +57,19 @@ density_ips = 785.1  # кг/м3
 density_co2 = 468  # кг/м3
 
 
-
 file_path = os.path.dirname(os.path.abspath(__file__))
 img_path = os.path.join(file_path, 'Images')
 
 num_steps = 100  # количество шагов
 l = np.empty(num_steps + 2, dtype=np.int16)
 
-
-proc_time = 15*3600
+proc_time = 15*360
 c_bound = 0.
-c_init = 1.
+c_init = 10.
 
 
 class scd_apparatus():
-    def __init__(self, width, length, height, diff_coef, key, key_sch, number_samples):
+    def __init__(self,volume, flowrate, width, length, height, diff_coef, key, key_sch, number_samples):
         self.width = width
         self.length = length
         self.height = height
@@ -81,17 +81,30 @@ class scd_apparatus():
         self.matrix_of_c = np.zeros((n_t, len(self.c_init_list)))
         self.list_of_mass = np.zeros(n_t)
         self.c_app = np.zeros(n_t)
+        self.volume = volume
+        self.flowrate =flowrate
 
+        if self.key == 'one_dim':
+            self.V_gels = self.height * self.width * self.length * self.number_samples
 
-        for i in range(num_steps + 2):
-            self.c_init_list[i] = c_init
-            if i == num_steps + 1:
-                self.c_init_list[i] = 0
+        elif self.key == 'cyl':
+            self.V_gels = self.number_samples * self.length * np.pi * R ** 2
+
+        elif self.key == 'sphere':
+            self.V_gels = self.number_samples * 4 / 3 * np.pi * (R) ** 3
+
+        self.V_apparat_free = self.volume - self.V_gels #объём свободного пространства аппарата
+        self.m_ips_gramms = self.V_gels * target_material_porosity * density_ips * 1000 #масса ипс
+
+        # for i in range(num_steps + 2):
+        #     self.c_init_list[i] = c_init
+        #     if i == num_steps + 1:
+        #         self.c_init_list[i] = 0
 
     def __str__(self):
         print(
             f'width: {self.width}, length: {self.length}, diff_coef: {self.diff_coef}, number_samples: {self.number_samples}')
-
+        print('Объём гелей', self.V_gels)
     """
     эта функция отвечает за создание коэффициента диффузии в зависимости от массовой доли
     """
@@ -103,6 +116,12 @@ class scd_apparatus():
         density = (ro / density_ips + (1 - ro) / density_co2) ** -1
         return density
 
+    def golden_method(self, x):
+        numerator = abs(
+            (self.V_apparat_free + self.V_gels * target_material_porosity) *
+            ((x / density_ips + (1 - x) / density_co2) ** -1) * x
+            - (self.m_ips_gramms / 1000))
+        return numerator / (self.m_ips_gramms / 1000)
 
     def fick_conc(self, c, c_bound, dr, dt, r):
         global sverka_method
@@ -144,47 +163,66 @@ class scd_apparatus():
 
         elif self.key == 'cyl':
             if self.key_sch == 'explicit':
-                if 2 * self.diff_coef * stab_cond <= 1:  # (2*diff_coef*dt-dt)/ dr**2 <= 1:     #diff_coef*(dt/dr + 2 * stab_cond) > 1:страница 84 методички
+                if dt> dr/(2 *self.diff_coef*porosity/tau_izv):
+                #if 2 * self.diff_coef * stab_cond <= 1:  # (2*diff_coef*dt-dt)/ dr**2 <= 1:     #diff_coef*(dt/dr + 2 * stab_cond) > 1:страница 84 методички
                     sverka_method = 5
                     pass
                 else:
                     for i in range(1, len(r) - 1):
-                        x_ips_past = (y_ips[i-1] * M_ips) / ((1 - y_ips[i-1]) * M_co2 + y_ips[i-1] * M_ips)
-                        x_ips_now = (y_ips[i] * M_ips) / ((1 - y_ips[i]) * M_co2 + y_ips[i] * M_ips)
-                        x_ips_fut = (y_ips[i+1] * M_ips) / ((1 - y_ips[i+1]) * M_co2 + y_ips[i+1] * M_ips)
+                        # y_start = optimize.golden(f, maxiter=10000)  # массовая доля, кг/кгсм
+                        #
+                        # V_ips = density_co2 * y_start / (density_ips * (1 - y_start) + density_co2 * y_start)
 
-                        density_past = self.density(y_ips[i-1])
-                        density_now = self.density(y_ips[i])
-                        density_fut = self.density(y_ips[i + 1])
-
-                        diff_coef_past = self.diffusion(x_ips_past)
-                        diff_coef_now = self.diffusion(x_ips_now)
-                        diff_coef_fut = self.diffusion(x_ips_fut)
-
-                        #c_temp[1:-1] = c[1:-1] + self.diff_coef * dt * ((c[2:] - 2 * c[1:-1] + c[0:-2]) / dr ** 2 + 1 / r[i] * (c[1:-1] - c[0:-2]) / dr)
-                        c_temp[1:-1] = c[1:-1] + porosity * dt / (tau_izv * dr * r[i]) * \
-                                   ((diff_coef_fut + diff_coef_now) / 2  * (density_fut + density_now) /2 * (r[i+1] + r[i]) / 2 * (
-                                            y_ips[i+1] - y_ips[i]) / dr - (
-                                            diff_coef_past + diff_coef_now) / 2 * (density_past + density_now) /2 * (r[i-1] + r[i]) / 2 * (
-                                            y_ips[i] - y_ips[i-1]) / dr)
+                        c_temp[1:-1] = c[1:-1] + self.diff_coef * (dt / dr ** 2) * (c[2:] - 2 * c[1:-1] + c[0: -2])
+                        # c_temp[1:-1] = c[1:-1] + porosity * dt / (tau_izv * dr * r[i]) * ((diff_coef_now * density_now * r[i] * (y_ips[i] - y_ips[i-1])/ dr) - \
+                        #                                                                    (diff_coef_past * density_past * r[i-1] * (y_ips[i] - y_ips[i-1])/ dr))
+                        # c_temp[1:-1] = c[1:-1] + porosity * dt / (tau_izv * dr * r[i]) * \
+                        #            ((diff_coef_fut + diff_coef_now) / 2  * (density_fut + density_now) /2 * (r[i+1] + r[i]) / 2 * (
+                        #                     y_ips[i+1] - y_ips[i]) / dr - (
+                        #                     diff_coef_past + diff_coef_now) / 2 * (density_past + density_now) /2 * (r[i-1] + r[i]) / 2 * (
+                        #                     y_ips[i] - y_ips[i-1]) / dr)
 
                     c_temp[-1] = c_bound
                     c_temp[0] = c_temp[1]
+
                     return c_temp
 
             elif self.key_sch == 'implicit':  # должна быть абсолютно устойчива это с ЛКР
-                for j in range(1, len(r)):
-                    a = -self.diff_coef * dt / (dr) ** 2
-                    b = 1 + 2 * dt * self.diff_coef / (dr) ** 2 - dt * self.diff_coef / (dr * r[j])
-                    c_koef = - dt * self.diff_coef / (dr) ** 2 + dt * self.diff_coef / (dr * r[j])
-                for i in range(1, len(l)):
-                    alfa_i[i] = (-a) / (b + c_koef * alfa_i[i - 1])
-                    betta_i[i] = (c[i] - c_koef * betta_i[i - 1]) / (b + c_koef * alfa_i[i - 1])
+                k_coef = dt * porosity / (tau_izv * dr)
+                # TODO надо как-то подумать, чтобы диапазон был до len(r)
+                for i in range(1, len(r) - 1):
+                    x_ips_past = (y_ips[i - 1] * M_ips) / ((1 - y_ips[i - 1]) * M_co2 + y_ips[i - 1] * M_ips)
+                    x_ips_now = (y_ips[i] * M_ips) / ((1 - y_ips[i]) * M_co2 + y_ips[i] * M_ips)
+                    x_ips_fut = (y_ips[i + 1] * M_ips) / ((1 - y_ips[i + 1]) * M_co2 + y_ips[i + 1] * M_ips)
 
+                    density_past = self.density(y_ips[i - 1])
+                    density_now = self.density(y_ips[i])
+                    density_fut = self.density(y_ips[i + 1])
+
+                    diff_coef_past = self.diffusion(x_ips_past)
+                    diff_coef_now = self.diffusion(x_ips_now)
+                    diff_coef_fut = self.diffusion(x_ips_fut)
+
+                    a_coef = - k_coef / r[i] * ((diff_coef_fut + diff_coef_now) / 2 * (r[i+1] + r[i]) / 2 * (density_fut +  density_now) / 2) / dr
+                    b_coef = 1 + k_coef / r[i] * ((diff_coef_fut + diff_coef_now) / 2 * (r[i+1] + r[i]) / 2 * (density_fut +  density_now) / 2) / dr  +  ((diff_coef_past + diff_coef_now) / 2 * (r[i-1] + r[i]) / 2 * (density_past +  density_now) / 2) / dr
+                    c_koef = -((diff_coef_past + diff_coef_now) / 2 * (r[i-1] + r[i]) / 2 * (density_past +  density_now) / 2) / dr
+
+                # for j in range(1, len(r)):
+                #     a = -self.diff_coef * dt / (dr) ** 2
+                #     b = 1 + 2 * dt * self.diff_coef / (dr) ** 2 - dt * self.diff_coef / (dr * r[j])
+                #     c_koef = - dt * self.diff_coef / (dr) ** 2 + dt * self.diff_coef / (dr * r[j])
+
+                for i in range(1, len(l) - 1):
+                    alfa_i[i] = (-a_coef) / (b_coef + c_koef * alfa_i[i - 1])
+                    betta_i[i] = (c[i] - c_koef * betta_i[i - 1]) / (b_coef + c_koef * alfa_i[i - 1])
+
+                # TODO тут проверить
                 alfa_i[-1] = 0
                 betta_i[-1] = 0
-                c_temp[1:-1] = c[2:] * alfa_i[1:-1] + betta_i[1:-1]
-                c_temp[-1] = c_bound
+                #c_temp[1:-1] = c[2:] * alfa_i[1:-1] + betta_i[1:-1]
+                c_temp = c[2:] * alfa_i[1:-1] + betta_i[1:-1]
+                c_temp.append(c_bound)
+                #c_temp[-1] = c_bound
                 c_temp[0] = c_temp[1]
                 return c_temp
 
@@ -236,8 +274,6 @@ class scd_apparatus():
         global method_value
         method_value = 0  # костыль для определения и не вылетания объёма аппарата
         c_app = np.zeros(n_t)
-        # volume = 0.00025 #кубические метры из диссертации
-        # flow_rate = 0.0000017 #кубических метров в секунду из диссертации
         residence_time = volume / flowrate
 
         c_matrix = np.zeros((n_t, len(c_init_list)))
@@ -250,15 +286,7 @@ class scd_apparatus():
             c_bound = c_app[i - 1]
             c_matrix[i] = self.fick_conc(c_matrix[i - 1], c_bound, dr, dt, r)
 
-            if volume < self.number_samples * 4 / 3 * np.pi * (R) ** 3 and self.key == 'sphere':
-                method_value = 5
-                pass
-
-            elif volume < self.number_samples * self.length * np.pi * R ** 2 and self.key == 'cyl':
-                method_value = 5
-                pass
-
-            elif volume < self.number_samples * self.length * self.width * R and self.key == 'one_dim':
+            if volume < self.V_gels:
                 method_value = 5
                 pass
 
@@ -290,7 +318,7 @@ def main(width, length, height, volume, flowrate, dt, diff_coef, number_samples,
             c_init_list[i] = 0
 
 
-    object1 = scd_apparatus(width, length, height, diff_coef, value, key_sch, number_samples)
+    object1 = scd_apparatus(volume, flowrate, width, length, height, diff_coef, value, key_sch, number_samples)
     object1.__str__()
 
     print('n_t:', n_t, 'proc_time:', proc_time, 'variable of item',value)
